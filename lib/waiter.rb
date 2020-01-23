18818f3fef6853ad3da4de9230ad64f4ff3dd6f5
Winifred Frierson <winifred.dykes@hotmail.com>
2020-01-23 15:18:15 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -28,12 +28,6 @@ class Waiter
     best_tipped_meal = meals.max do |meal_a, meal_b|
       meal_a.tip <=> meal_b.tip
     end
-  end
-  
-  def best_tipper
-    best_tipped_meal = meals.max do |meal_a, meal_b|
-      meal_a.tip <=> meal_b.tip
-    end
     
     best_tipped_meal.customer
     
